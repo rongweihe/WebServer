eec27ca39d361b4a3a481e8a1d8b2e6afa3e89f7
herongwei <herongwei@sogou-inc.com>
2020-11-09 16:35:08 +0800

    update threadpool desc

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,8 +1,23 @@
-/********************************************************
-*@file   ThreadPool.cpp
-*@Author rongweihe
-*@Data   2019/03/31
-*********************************************************/
+/*
+  Copyright (c) 2019 rongweihe, Inc.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  Author: rongweihe (rongweihe1995@gmail.com)
+  Data:   2019/03/31
+  update: 2020/11/09
+*/
+
 #include "ThreadPool.h"
 #include <bits/stdc++.h>
 #include <pthread.h>
@@ -25,6 +40,8 @@ ThreadPool::ThreadPool(int thread_s, int max_queue_s) : max_queue_size(max_queue
     threads.resize(m_thread_size);
 
     //创建线程
+    //C API:pthread_create 
+    //C++ 11 提供 std::thread
     for(int i=0; i<m_thread_size; ++i)
     {
         if(pthread_create(&threads[i],NULL,worker,this)!=0 )
@@ -52,8 +69,7 @@ bool ThreadPool::append(std::shared_ptr<void> arg, std::function<void(std::share
     MutexLockGuard guard(this->m_mutex);
     if (request_queue.size() > max_queue_size)
     {
-        std::cout << max_queue_size;
-        std::cout << "ThreadPool too many requests" << std::endl;
+        std::cout << "ThreadPool too many requests" << max_queue_size <<std::endl;
         return false;
     }
     //入队操作
